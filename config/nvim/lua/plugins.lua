5d43b76021e7e1c2fb528d0c8c4c65825ee3eb65
William Houston <William.Houston@valencepm.com>
2022-01-06 09:54:34 -0500

    Add tpope/vim-fugitive for git in vim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,9 +1,11 @@
 return require('packer').startup(function()
   -- Packer can manage itself
-  use { "wbthomason/packer.nvim" }
+  use { 'wbthomason/packer.nvim' }
   use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}} }
   -- telescope optional dependecy to improve sorting performance
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   -- telescope optional dependency for preview
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  use { 'tpope/vim-fugitive' }
 end)
