ff751b3542a0f01868e5bd7133ee9593e1ac7628
William Houston <William.Houston@valencepm.com>
2022-03-11 16:39:13 -0500

    working server restart with slightly more robust lookup

--- config/nvim/lua/tools.lua
+++ config/nvim/lua/tools.lua
@@ -2,51 +2,75 @@ local function t(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
+local barcoder_server_buf_name = 'Barcoder Server'
+local buf_job_var = 'terminal_job_id'
+local docs_server_buf_name = 'Docs Server'
+local rails_console_server = 'Rails Console'
 local rerun_text = t'<c-c>!!\n\n'
-local docs_server_buf = 'Docs Server'
+local start_barcoder = t'cd ~/Code/val-barcoder\nforedev\n'
+local start_docs = t'cd ~/Code/val-doc-storage\nforedev\n'
+local start_rc = t'rc\n'
 
 local function send(job_id, command)
   vim.call('jobsend', job_id, command)
 end
 
-local function start_servers()
-  vim.api.nvim_command('vsp')
-  vim.api.nvim_command('terminal')
-  barcoder_job = vim.b.terminal_job_id
-  send(barcoder_job, 'cd ../val-barcoder\n')
-  send(barcoder_job, 'foredev\n')
-  vim.api.nvim_command('vsp')
-  vim.api.nvim_command('terminal')
-  docs_job = vim.b.terminal_job_id
-  send(docs_job, 'foredev\n')
-  vim.api.nvim_command('q')
-  vim.api.nvim_command('q')
+local function open_terminal()
+  vim.call('termopen', '/bin/zsh')
 end
 
-local function open_terminal()
-  vim.api.nvim_command('terminal')
+local function terminal_id_from_buf_name(buf_name)
+  local buf, buf_exists, job
+  buf_exists = vim.call('bufexists', buf_name)
+
+  if buf_exists ~= 0 then
+    buf = vim.call('bufadd', buf_name)
+  else
+    buf = vim.api.nvim_create_buf(true, false)
+    vim.api.nvim_buf_call(buf, open_terminal)
+    vim.api.nvim_buf_set_name(buf, buf_name)
+  end
+
+  return vim.call('getbufvar', buf, buf_job_var)
+end
+
+local function start_server_if_missing(buf_name, send_text)
+  local buf_exists = vim.call('bufexists', buf_name)
+
+  if buf_exists == 0 then
+    term_id = terminal_id_from_buf_name(buf_name)
+    send(term_id, send_text)
+    return true
+  end
+
+  return false
 end
 
 function _G.restart_server()
-  docs_buf = vim.call('bufadd', docs_server_buf)
-  docs_job = vim.call('getbufvar', docs_buf, 'terminal_job_id')
-
-  if !docs_job then
-    vim.api.nvim_buf_call(docs_buf, open_terminal())
-  end
-
-  docs_job = vim.call('getbufvar', docs_buf, 'terminal_job_id')
-
-  if docs_job then
-    send(docs_job, rerun_text)
-    print('server restarted')
+  start_server_if_missing(barcoder_server_buf_name, start_barcoder)
+  if start_server_if_missing(docs_server_buf_name, start_docs) == false then
+    send(terminal_id_from_buf_name(docs_server_buf_name), rerun_text)
+    print('restarted server')
   else
-    start_servers()
-    print('servers started')
+    print('started server')
   end
 end
 
+local function goto_server(buf_name, send_text)
+  start_server_if_missing(buf_name, send_text)
+
+  buf = vim.call('bufadd', buf_name)
+
+  vim.api.nvim_win_set_buf(0, buf)
+end
+
+function _G.goto_rc()
+  goto_server(rails_console_server, start_rc)
+end
+
 nmap(t'<leader>rs', t':lua restart_server()<cr>')
+nmap(t'<leader>rc', t':lua goto_rc()<cr>')
+nmap(t'<leader>gs', t':lua restart_server()<cr>')
 -- nvim_open_term(buffer, opts)
 -- nvim_chan_send(channel, bytes)
 -- nvim_replace_termcodes()
