41505c642461c3fe02459c74f7f0775352250e0a
William Houston <william.houston@valencedocs.com>
2023-03-27 16:42:10 -0400

    [f] Add vim-ruby (necessary?) and remove '.' from ruby indentkeys

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,3 +5,14 @@ require('config.key_maps')
 require('config.nvim_settings')
 require('tools')
 require('snippets')
+
+-- HACK HACK HACK
+-- Treesitter was reindenting with '.', and I couldn't figure out an lua-ish way
+-- to remove it from indentkeys.
+-- https://github.com/nvim-treesitter/nvim-treesitter/issues/2566
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { "ruby" },
+  callback = function()
+    vim.cmd[[setlocal indentkeys-=.]]
+  end
+})
