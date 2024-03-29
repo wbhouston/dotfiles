427bb1cce3ffbee4236fca4027289edea35373de
William Houston <william.houston@valencedocs.com>
2024-03-29 19:35:15 -0400

    Add slim tag toggle locally

--- config/nvim/lua/custom/plugins/lualine/init.lua
+++ config/nvim/lua/custom/plugins/lualine/init.lua
@@ -21,7 +21,7 @@ return {
         lualine_c = {},
         lualine_x = {},
         lualine_y = {},
-        lualine_z = {},
+        lualine_z = { 'location' },
       },
       inactive_sections = {
         lualine_a = {
---
+++ config/nvim/lua/custom/plugins/slim-tag-toggle/init.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'val-whouston/slim-tag-toggle.nvim',
+    config = {
+      debug = false,
+    },
+    dev = true,
+  },
+}
