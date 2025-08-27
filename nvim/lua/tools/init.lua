1518cb83459971edf81fb3688a3cf6d9d52a28b0
William Houston <wmbhouston@gmail.com>
2025-08-27 13:58:06 -0400

    Add a goto function for PKM and break out markview to configure headings
    markview is complaining about being loaded after treesitter, so i dunno

--- nvim/lua/tools/init.lua
+++ nvim/lua/tools/init.lua
@@ -1,6 +1,58 @@
+function PKMGoto()
+  local line = vim.fn.getline('.')
+  local pos = vim.fn.getpos('.')
+  local column = pos[3]
+  local search_within_string = string.sub(line, 0, column)
+  local full_string_search = false
+  local init = 1
+  local link = nil
+
+  while(init and init <= column) do
+    local internal_link_start, _ = string.find(search_within_string, '%[%[', init)
+
+    if internal_link_start then
+      local full_start, full_end = string.find(line, '%b[]', internal_link_start)
+
+      if not full_string_search and full_start <= column and column <= full_end then
+        -- Found a link under the cursor
+        link = string.sub(line, full_start + 2, full_end - 2)
+        break
+      elseif full_string_search and full_start then
+        -- We are not in a link, but this line has a link. Going there.
+        link = string.sub(line, full_start + 2, full_end - 2)
+        break
+      else
+        -- This link start did not have a link end.
+        init = full_end + 1
+      end
+    else
+      if full_string_search then
+        -- We did not find a link anywhere.
+        break
+      else
+        -- The cursor was not under a link, search the full string.
+        search_within_string = line
+        full_string_search = true
+        init = 1
+      end
+    end
+  end
+
+  if link then
+    print('link', link)
+    local filepath = link .. '.md'
+    local f = io.open(filepath,"r")
+    if f ~= nil then
+      io.close(f)
+      vim.cmd("vi " .. filepath)
+    end
+  end
+end
+
 function PKMSettings()
   vim.o.columns = 200
   vim.o.linebreak = true
+  vim.o.colorcolumn = '0'
 
   vim.cmd([[NERDTree]])
   vim.cmd([[wincmd l]])
@@ -10,3 +62,4 @@ function PKMSettings()
 end
 
 nmap("<leader>pkm", ":lua PKMSettings()<cr>", "Open PKM index with NERDTree and Aerial")
+nmap("<leader>pkg", ":lua PKMGoto()<cr>", "Open PKM file under cursor")
