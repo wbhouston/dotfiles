0340e07865ef40f2e7e1b4c779fc45f6e60b987c
William Houston <william.houston@valencedocs.com>
2026-01-21 11:11:54 -0500

    Add AI and specify colorscheme

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,12 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    opts = {
+      log_level = "DEBUG", -- or "TRACE"
+    },
+  },
+}
