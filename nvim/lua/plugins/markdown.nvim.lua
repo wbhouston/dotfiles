76692fcb100605df5018f61eb3d3eb9b08a26deb
William Houston <william.houston@valencedocs.com>
2026-02-03 08:40:13 -0500

    Save before updating

--- nvim/lua/plugins/markdown.nvim.lua
+++ nvim/lua/plugins/markdown.nvim.lua
@@ -1,15 +1,7 @@
 return {
   "tadmccorkle/markdown.nvim",
   ft = "markdown", -- or 'event = "VeryLazy"'
-  keys = {
-    {
-      "<cr>",
-      "<cmd>MDListItemBelow<cr>",
-      mode = { "i" },
-    },
-  },
   opts = {
     -- configuration here or empty for defaults
   },
 }
-
